a4519cd942686e7af16f7a9ceca7e70dcd0caf53
SlavishaStojanoski <slavisa.stojanoski@gmail.com>
2021-05-12 20:31:03 +0200

    des

--- Developer-skills/JavaScript-Vezbi/index.js
+++ Developer-skills/JavaScript-Vezbi/index.js
@@ -1,3 +1,4 @@
+'use strict';
 // TODO = DONE
 // Chalange from javascript udemy course for debuging
 
@@ -51,3 +52,111 @@
 // function deleteShopingCards() {
 //   console.log('All Cards is deleted');
 // }
+
+// Destructuring
+// const arr = [2, 3, 4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+// console.log(a, b, c);
+
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+
+// Destructuring array DONE
+// const restaurant = {
+
+//   name: 'pizza3',
+//   location: 'Gostivar',
+//   categories: ['Italiana', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruchetta', 'Grlic Bread', 'Capresa'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risoto'],
+
+//   order: function (starterIndex, mainIndex) {
+//     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+//   },
+// };
+
+// let [main, secondary] = restaurant.categories;
+// console.log(main, secondary);
+
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
+
+// const [starter, mainMenu] = restaurant.order(3, 2);
+// console.log(starter, mainMenu);
+
+// Nested Destructuring
+// let nest = [2, 4, [8, 10, [11, 12]]];
+// nest[1] = 5;
+// let [a, b, [c, d, [e, f]]] = nest;
+// console.log(a, b, c, d, e, f);
+
+// Destructuring OBJECT DONE
+// const restaurant = {
+//   name: 'pizza3',
+//   location: 'Gostivar',
+//   categories: ['Italiana', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruchetta', 'Grlic Bread', 'Capresa'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risoto'],
+//   openingHours: {
+//     thu: {
+//       open: 12,
+//       close: 22,
+//     },
+//     fri: {
+//       open: 11,
+//       close: 23,
+//     },
+//     sat: {
+//       open: 0, //open 24hours
+//       close: 24,
+//     },
+//   },
+
+//   order: function (starterIndex, mainIndex) {
+//     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+//   },
+//   orderDelivery: function ({ time, address, starterIndex, mainIndex }) {
+//     console.log(
+//       time,
+//       address,
+//       `${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]}`
+//     );
+//   },
+// };
+
+// restaurant.orderDelivery({
+//   time: '22:10',
+//   address: 'Beogradska bb',
+//   mainIndex: 1,
+//   starterIndex: 3,
+// });
+
+// const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
+
+// const {
+//   name: restaurantName,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
+// console.log(restaurantName, hours, tags);
+
+// // Default values
+// const { menu = [], starterMenu: start = [] } = restaurant;
+// console.log(menu, start);
+
+// // Mutating Variables
+// let a = 10;
+// let b = 15;
+
+// const newObj = { a: 22, b: 11, c: 55 };
+// ({ a, b } = newObj);
+// console.log(a, b);
+
+// // Nested Object
+// const {
+//   fri: { open: o, close: c },
+// } = hours; //hours become from desructured object restaurant like a value of properti openingHours
+// console.log(o, c);
