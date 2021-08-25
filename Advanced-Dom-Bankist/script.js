952cc795aa407216743236e8e075dcbe9280793f
SlavishaStojanoski <slavisa.stojanoski@gmail.com>
2021-08-25 21:22:31 +0200

    .

--- Advanced-Dom-Bankist/script.js
+++ Advanced-Dom-Bankist/script.js
@@ -76,3 +76,61 @@ nav.addEventListener(
 // All Code is still in javaScript/stickyNav.js file
 
 //NEW REVEALING elemnts in scroll
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  // console.log(entry);
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+const sectionsObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionsObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
+//NEW Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+
+  //Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '-200px',
+});
+
+imgTargets.forEach((img) => imgObserver.observe(img));
+
+//Sticky navigation: intersection Observer API
+
+const obsCallBack = function (entries, observer) {
+  entries.forEach((entry) => {
+    console.log(entry);
+  });
+};
+
+const obsOptions = {
+  root: null,
+  threshold: 0.1,
+};
+const observer = new IntersectionObserver(obsCallBack, obsOptions);
+observer.observe(section1);
